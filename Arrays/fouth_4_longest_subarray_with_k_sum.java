a39e02a0f8e10379128d3cb5ded964f1a783b843
Abhishek Dwivedi <abhishekdwivedi8109@gmail.com>
2024-10-05 17:22:36 +0530

    commit

--- Arrays/fouth_4_longest_subarray_with_k_sum.java
+++ Arrays/fouth_4_longest_subarray_with_k_sum.java
@@ -25,6 +25,24 @@ public class fouth_4_longest_subarray_with_k_sum {
         }
         return max;
     }
+
+    static int approach_secound(int arr[], int k){
+		HashMap<Integer, Integer> map = new HashMap<>();
+		int sum = 0, max = 0;
+		for(int i = 0; i < arr.length; i ++){
+			sum += arr[i];
+			if(sum == k){
+				max = Math.max(max, i + 1);
+			}
+			if(map.containsKey(sum - k)){
+				max = Math.max(max, i - map.get(sum - k));
+			}else if(!map.containsKey(sum)){
+				map.put(sum, i);
+			}
+			
+		}
+		return max;
+	}
     public static void main(String[] args) {
         int arr[] = {1, 2, 1, 0, 1 };
         int k = 5;
